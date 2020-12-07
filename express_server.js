b55972512712bebcc5ebd97d92cb8e3b2d4ba97a
Nisarg <nisargvv@gmail.com>
2020-12-07 22:55:50 +0000

    Created a new POST method

--- express_server.js
+++ express_server.js
@@ -9,6 +9,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  const str = Math.random().toString(36).substr(2,6);
+  return str;
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -30,9 +38,23 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  console.log(longURL);
+  res.redirect(longURL);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  let url = req.body.longURL;
+  let randomString = generateRandomString();
+  urlDatabase[randomString] = url;
+  console.log(urlDatabase);
+  res.redirect(`/urls/:${randomString}`);
 });
 
 app.listen(PORT, () => {
